155c97df4c94b8c47e1304874e67d075f52b75c5
unknown <xvinh.ir@gmail.com>
2020-10-03 15:23:54 +0700

    added edit boss

--- routes/bosses.js
+++ routes/bosses.js
@@ -1,10 +1,11 @@
 var express = require("express");
-const { getAllBosses, createBoss, getSingleBoss } = require("../controllers/bossController");
+const { getAllBosses, createBoss, getSingleBoss, updateBoss } = require("../controllers/bossController");
 
 var router = express.Router();
 
 router.route("/boss").get(getAllBosses);
 router.route("/boss/:name").get(getSingleBoss);
+router.route("/boss/:name").put(updateBoss);
 router.route("/boss/create").post(createBoss);
 
 module.exports = router;

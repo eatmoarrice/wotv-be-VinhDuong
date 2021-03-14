931d5fed97e9b92040ddd89d80c27279d012a313
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2021-03-14 23:50:55 +0700

    add html view

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-require("dotenv").config({ path: ".env" });
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config({ path: '.env' });
 var app = express();
-const mongoose = require("mongoose");
-var cors = require("cors");
-const passport = require("passport");
-require("./helpers/passport");
-const indexRouter = require("./routes");
-const utilsHelper = require("./helpers/utilsHelper");
+const mongoose = require('mongoose');
+var cors = require('cors');
+const passport = require('passport');
+require('./helpers/passport');
+const indexRouter = require('./routes');
+const utilsHelper = require('./helpers/utilsHelper');
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(passport.initialize());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", cors(), indexRouter);
+app.use('/', cors(), indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,14 +32,14 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-	console.log("ERROR", err);
+	console.log('ERROR', err);
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render("error");
+	res.render('error');
 });
 
 mongoose
@@ -47,12 +47,12 @@ mongoose
 		useCreateIndex: true,
 		useNewUrlParser: true,
 		useFindAndModify: false,
-		useUnifiedTopology: true
+		useUnifiedTopology: true,
 	})
 	.then(() => {
 		console.log(`Mongoose connected to DB`);
 		// require("./faker.js");
 	})
-	.catch((err) => console.error("Could not connect to database!", err));
+	.catch((err) => console.error('Could not connect to database!', err));
 
 module.exports = app;
